56b000e65315ebfdd5746d702a8b20a9bfa22396
Maurice Amon <amon@zhaw.ch>
2025-04-21 14:39:14 +0200

    Implemented a new datastructure for updating the client

--- commands/FetchGameStateCommand.py
+++ commands/FetchGameStateCommand.py
@@ -2,11 +2,38 @@
 
 @author Maurice Amon
 '''
+import pickle
 from commands.Command import Command
 from commands.requests.FetchGameStateRequest import FetchGameStateRequest
+from commands.responses.GameStateResponse import GameStateResponse
+from model.board.BattleshipMatrix import BattleshipMatrix
 
 
 class FetchGameStateCommand(Command):
 
+    _fetch_request = None
+
+    _conn = None
+
     def __init__(self, request: FetchGameStateRequest, conn):
-        pass
+        print("Heeeeloooooooo")
+        self._fetch_request = request
+        self._conn = conn
+
+
+    def execute(self):
+        self.update_client()
+
+
+    def update_client(self):
+        battleship_matrix = BattleshipMatrix()
+        battleship_matrix.create_battleships()
+        battleship_matrix2 = BattleshipMatrix()
+        battleship_matrix2.create_battleships()
+        game_state = GameStateResponse(battleship_matrix, battleship_matrix2, 0, self._fetch_request)
+
+        message = pickle.dumps(game_state)
+        self._conn.send(message)
+        print("Updated Battleship matrix has been sent to the client: ")
+        battleship_matrix.print_matrix()
+        self._conn.close()
